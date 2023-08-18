6eb9ac71bd9cf1fb7f3c8447de46790fc329cfb4
dimden <admin@dimden.dev>
2023-08-18 19:36:22 +0300

    attempt to fix

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if(request.action === "setcookie") {
+        let ct0 = request.cookie.match(/(?:^|;\s*)ct0=([0-9a-f]+)\s*(?:;|$)/);
+        if(!ct0) return;
+        ct0 = ct0[1];
         chrome.cookies.set({
             domain: ".twitter.com",
             name: "ct0",
-            value: request.cookie.match(/(?:^|;\s*)ct0=([0-9a-f]+)\s*(?:;|$)/)[1],
+            value: ct0,
             url: "https://tweetdeck.twitter.com/",
             secure: true,
             sameSite: "no_restriction"
@@ -11,7 +14,7 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
         chrome.cookies.set({
             domain: ".dimden.dev",
             name: "ct0",
-            value: request.cookie.match(/(?:^|;\s*)ct0=([0-9a-f]+)\s*(?:;|$)/)[1],
+            value: ct0,
             url: "https://tweetdeck.dimden.dev/",
             secure: true,
             sameSite: "no_restriction"

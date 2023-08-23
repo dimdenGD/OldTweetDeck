eb9b314ac113db88123506bd0e398988bba15df7
dimden <admin@dimden.dev>
2023-08-24 00:06:16 +0300

    fix user search

--- src/interception.js
+++ src/interception.js
@@ -330,6 +330,71 @@ const proxyRoutes = [
                 modules: res.map(t => ({status: {data: t}}))
             };
         }
+    },
+    {
+        path: '/1.1/users/search.json',
+        method: 'GET',
+        beforeRequest: xhr => {
+            xhr.modReqHeaders['Authorization'] = PUBLIC_TOKEN;
+            try {
+                let url = new URL(xhr.modUrl);
+                let params = new URLSearchParams(url.search);
+                let variables = {
+                    rawQuery: params.get('q'),
+                    count: 20,
+                    querySource: 'typed_query',
+                    product: "People",
+                };
+                let features = {"rweb_lists_timeline_redesign_enabled":false,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false};
+
+                xhr.modUrl = `${NEW_API}/nK1dw4oV3k4w5TdtcAdSww/SearchTimeline?${generateParams(features, variables)}`;
+            } catch(e) {
+                console.error(e);
+            }
+        },
+        afterRequest: xhr => {
+            try {
+                data = JSON.parse(xhr.responseText);
+            } catch(e) {
+                console.error(e);
+                return [];
+            }
+            if (data.errors && data.errors[0]) {
+                return [];
+            }
+            let instructions = data.data.search_by_raw_query.search_timeline.timeline.instructions;
+            let entries = instructions.find(i => i.entries);
+            if(!entries) {
+                return [];
+            }
+            entries = entries.entries;
+            let res = [];
+            for(let entry of entries) {
+                if(entry.entryId.startsWith('sq-I-u-') || entry.entryId.startsWith("user-")) {
+                    let result = entry.content.itemContent.user_results.result;
+                    if(!result || !result.legacy) {
+                        console.log("Bug: no user", entry);
+                        continue;
+                    }
+                    let user = result.legacy;
+                    user.id_str = result.rest_id;
+                    res.push(user);
+                }
+            }
+            let cursor = entries.find(e => e.entryId.startsWith('sq-cursor-bottom-') || e.entryId.startsWith('cursor-bottom-'));
+            if(cursor) {
+                cursor = cursor.content.value;
+            } else {
+                cursor = instructions.find(e => e.entry_id_to_replace && (e.entry_id_to_replace.startsWith('sq-cursor-bottom-') || e.entry_id_to_replace.startsWith('cursor-bottom-')));
+                if(cursor) {
+                    cursor = cursor.entry.content.value;
+                } else {
+                    cursor = null;
+                }
+            }
+
+            return res;
+        }
     }
 ];
 

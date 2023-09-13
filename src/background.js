fdd32ad9052013a55a2225ddfc17c41a96f2de97
haxibami <me@haxibami.net>
2023-09-13 14:17:01 +0900

    block another Web App resource path

--- src/background.js
+++ src/background.js
@@ -98,7 +98,8 @@ chrome.webRequest.onBeforeRequest.addListener(
             // want to use details.originUrl but it's not available in Chrome
             let requestFrom = urls[details.tabId][details.frameId];
             if (
-                path.startsWith("/responsive-web/client-web/") &&
+                (path.startsWith("/responsive-web/client-web-legacy/") ||
+                    path.startsWith("/responsive-web/client-web/")) &&
                 requestFrom === "https://twitter.com/i/tweetdeck"
             ) {
                 return {

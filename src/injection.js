1eb2bd9816ad87c41748dcf0f29c6d7b423d4f47
dimden <admin@dimden.dev>
2023-09-20 19:36:33 +0300

    use newest twitter-text version and update the way getting remote files works

--- src/injection.js
+++ src/injection.js
@@ -2,75 +2,100 @@
     let html = await fetch(chrome.runtime.getURL('/files/index.html')).then(r => r.text());
     document.documentElement.innerHTML = html;
 
-    let [interception_js, vendor_js, bundle_js, bundle_css] =
+    let [interception_js, vendor_js, bundle_js, bundle_css, twitter_text] =
         await Promise.allSettled([
-            fetch(chrome.runtime.getURL("/src/interception.js")).then((r) =>
-                r.text(),
-            ),
-            fetch(chrome.runtime.getURL("/files/vendor.js")).then((r) =>
-                r.text(),
-            ),
-            fetch(chrome.runtime.getURL("/files/bundle.js")).then((r) =>
-                r.text(),
-            ),
-            fetch(chrome.runtime.getURL("/files/bundle.css")).then((r) =>
-                r.text(),
-            ),
+            fetch(chrome.runtime.getURL("/src/interception.js")).then(r => r.text()),
+            fetch(chrome.runtime.getURL("/files/vendor.js")).then(r => r.text()),
+            fetch(chrome.runtime.getURL("/files/bundle.js")).then(r => r.text()),
+            fetch(chrome.runtime.getURL("/files/bundle.css")).then(r => r.text()),
+            fetch(chrome.runtime.getURL("/files/twitter-text.js")).then(r => r.text()),
         ]);
     if (!localStorage.getItem("OTDalwaysUseLocalFiles")) {
         const [
-            remote_interception_js,
-            remote_vendor_js,
-            remote_bundle_js,
-            remote_bundle_css,
+            remote_interception_js_req,
+            remote_vendor_js_req,
+            remote_bundle_js_req,
+            remote_bundle_css_req,
+            remote_twitter_text_req,
         ] = await Promise.allSettled([
-            fetch(
-                "https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/src/interception.js",
-            ).then((r) => r.text()),
-            fetch(
-                "https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/files/vendor.js",
-            ).then((r) => r.text()),
-            fetch(
-                "https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/files/bundle.js",
-            ).then((r) => r.text()),
-            fetch(
-                "https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/files/bundle.css",
-            ).then((r) => r.text()),
+            fetch("https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/src/interception.js"),
+            fetch("https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/files/vendor.js"),
+            fetch("https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/files/bundle.js"),
+            fetch("https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/files/bundle.css"),
+            fetch("https://raw.githubusercontent.com/dimdenGD/OldTweetDeck/main/files/twitter-text.js"),
         ]);
+        
+        if(
+            remote_interception_js_req.value.ok || 
+            remote_vendor_js_req.value.ok || 
+            remote_bundle_js_req.value.ok || 
+            remote_bundle_css_req.value.ok || 
+            remote_twitter_text_req.value.ok
+        ) {
+            const [
+                remote_interception_js,
+                remote_vendor_js,
+                remote_bundle_js,
+                remote_bundle_css,
+                remote_twitter_text,
+            ] = await Promise.allSettled([
+                remote_interception_js_req.value.text(),
+                remote_vendor_js_req.value.text(),
+                remote_bundle_js_req.value.text(),
+                remote_bundle_css_req.value.text(),
+                remote_twitter_text_req.value.text(),
+            ]);
 
-        if (
-            remote_interception_js.status === "fulfilled" &&
-            remote_interception_js.value.length > 30
-        ) {
-            interception_js = remote_interception_js;
-            console.log("Using remote interception.js");
-        }
-        if (
-            remote_vendor_js.status === "fulfilled" &&
-            remote_vendor_js.value.length > 30
-        ) {
-            vendor_js = remote_vendor_js;
-            console.log("Using remote vendor.js");
-        }
-        if (
-            remote_bundle_js.status === "fulfilled" &&
-            remote_bundle_js.value.length > 30
-        ) {
-            bundle_js = remote_bundle_js;
-            console.log("Using remote bundle.js");
-        }
-        if (
-            remote_bundle_css.status === "fulfilled" &&
-            remote_bundle_css.value.length > 30
-        ) {
-            bundle_css = remote_bundle_css;
-            console.log("Using remote bundle.css");
+            if (
+                remote_interception_js_req.value.ok &&
+                remote_interception_js.status === "fulfilled" &&
+                remote_interception_js.value.length > 30
+            ) {
+                interception_js = remote_interception_js;
+                console.log("Using remote interception.js");
+            }
+            if (
+                remote_vendor_js_req.value.ok &&
+                remote_vendor_js.status === "fulfilled" &&
+                remote_vendor_js.value.length > 30
+            ) {
+                vendor_js = remote_vendor_js;
+                console.log("Using remote vendor.js");
+            }
+            if (
+                remote_bundle_js_req.value.ok &&
+                remote_bundle_js.status === "fulfilled" &&
+                remote_bundle_js.value.length > 30
+            ) {
+                bundle_js = remote_bundle_js;
+                console.log("Using remote bundle.js");
+            }
+            if (
+                remote_bundle_css_req.value.ok &&
+                remote_bundle_css.status === "fulfilled" &&
+                remote_bundle_css.value.length > 30
+            ) {
+                bundle_css = remote_bundle_css;
+                console.log("Using remote bundle.css");
+            }
+            if (
+                remote_twitter_text_req.value.ok &&
+                remote_twitter_text.status === "fulfilled" &&
+                remote_twitter_text.value.length > 30
+            ) {
+                twitter_text = remote_twitter_text;
+                console.log("Using remote twitter-text.js");
+            }
         }
     }
 
     let interception_js_script = document.createElement("script");
     interception_js_script.innerHTML = interception_js.value;
     document.head.appendChild(interception_js_script);
+
+    let bundle_css_style = document.createElement("style");
+    bundle_css_style.innerHTML = bundle_css.value;
+    document.head.appendChild(bundle_css_style);
 
     let vendor_js_script = document.createElement("script");
     vendor_js_script.innerHTML = vendor_js.value;
@@ -80,9 +105,9 @@
     bundle_js_script.innerHTML = bundle_js.value;
     document.head.appendChild(bundle_js_script);
 
-    let bundle_css_style = document.createElement("style");
-    bundle_css_style.innerHTML = bundle_css.value;
-    document.head.appendChild(bundle_css_style);
+    let twitter_text_script = document.createElement("script");
+    twitter_text_script.innerHTML = twitter_text.value;
+    document.head.appendChild(twitter_text_script);
 
     let int = setTimeout(function() {
         let badBody = document.querySelector('body:not(#injected-body)');

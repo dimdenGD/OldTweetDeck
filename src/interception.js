15f71ce00c3758e3d417e942376482340326a61f
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-07-02 22:15:46 +0300

    update reply api

--- src/interception.js
+++ src/interception.js
@@ -1108,7 +1108,7 @@ const proxyRoutes = [
         path: "/1.1/statuses/update.json",
         method: "POST",
         beforeRequest: (xhr) => {
-            xhr.modUrl = `https://${location.hostname}/i/api/graphql/tTsjMKyhajZvK4q76mpIBg/CreateTweet`;
+            xhr.modUrl = `https://${location.hostname}/i/api/graphql/oB-5XsHNAbjvARJEc8CZFw/CreateTweet`;
         },
         beforeSendHeaders: (xhr) => {
             xhr.modReqHeaders["Content-Type"] = "application/json";
@@ -1125,11 +1125,6 @@ const proxyRoutes = [
                     media_entities: [],
                     possibly_sensitive: false,
                 },
-                withDownvotePerspective: false,
-                withReactionsMetadata: false,
-                withReactionsPerspective: false,
-                withSuperFollowsTweetFields: true,
-                withSuperFollowsUserFields: true,
                 semantic_annotation_ids: [],
                 dark_request: false,
             };
@@ -1154,31 +1149,8 @@ const proxyRoutes = [
 
             return JSON.stringify({
                 variables,
-                features: {
-                    tweetypie_unmention_optimization_enabled: true,
-                    responsive_web_edit_tweet_api_enabled: true,
-                    graphql_is_translatable_rweb_tweet_is_translatable_enabled: true,
-                    view_counts_everywhere_api_enabled: true,
-                    longform_notetweets_consumption_enabled: true,
-                    responsive_web_twitter_article_tweet_consumption_enabled: false,
-                    tweet_awards_web_tipping_enabled: false,
-                    longform_notetweets_rich_text_read_enabled: true,
-                    longform_notetweets_inline_media_enabled: true,
-                    responsive_web_graphql_exclude_directive_enabled: true,
-                    verified_phone_label_enabled: false,
-                    freedom_of_speech_not_reach_fetch_enabled: true,
-                    standardized_nudges_misinfo: true,
-                    tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled: true,
-                    responsive_web_media_download_video_enabled: false,
-                    responsive_web_graphql_skip_user_profile_image_extensions_enabled: false,
-                    responsive_web_graphql_timeline_navigation_enabled: true,
-                    responsive_web_enhance_cards_enabled: false,
-                },
-                fieldToggles: {
-                    withArticleRichContentState: false,
-                    withAuxiliaryUserLabels: false,
-                },
-                queryId: "tTsjMKyhajZvK4q76mpIBg",
+                features: {"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"articles_preview_enabled":true,"rweb_video_timestamps_enabled":true,"rweb_tipjar_consumption_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_enhance_cards_enabled":false},
+                queryId: "oB-5XsHNAbjvARJEc8CZFw",
             });
         },
         afterRequest: (xhr) => {
